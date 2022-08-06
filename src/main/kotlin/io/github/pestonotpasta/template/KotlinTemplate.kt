e3c6b505fc38e5e02651bd4661097741122e7168
_Pesto <pestonotpasta@gmail.com>
2022-08-07 01:19:48 +0300

    Init commit

---
+++ src/main/kotlin/io/github/pestonotpasta/template/KotlinTemplate.kt
@@ -0,0 +1,14 @@
+package io.github.pestonotpasta.template
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class KotlinTemplate : JavaPlugin() {
+
+    override fun onEnable() {
+        // Override function
+    }
+
+    override fun onDisable() {
+        // Override function
+    }
+}
